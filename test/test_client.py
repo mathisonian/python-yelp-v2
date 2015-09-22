3df40d07f7116815b3a47aee6b0dbeb5ef9165a6
Edwin Grubbs <edwin@grubbs.org>
2015-09-22 11:18:26 -0500

    Fixes #14 Handle unicode parameter to GetBusiness().

--- test/test_client.py
+++ test/test_client.py
@@ -1,5 +1,6 @@
 import pytest
 import yelp
+import urllib
 
 print 'wtf'
 
@@ -25,6 +26,37 @@ class TestYelpAPIClient(object):
         business = client.GetBusiness('post-no-bills-brooklyn')
         assert business.name == 'Post No Bills'
 
+    def test_business_unicode(self, key, secret, token, token_secret):
+        client = yelp.Api(consumer_key=key,
+                          consumer_secret=secret,
+                          access_token_key=token,
+                          access_token_secret=token_secret)
+
+
+        business = client.GetBusiness(u'red-cat-jazz-caf\xe9-houston-3')
+        assert business.name == u'Red Cat Jazz Caf\xe9'
+
+    def test_business_utf8(self, key, secret, token, token_secret):
+        client = yelp.Api(consumer_key=key,
+                          consumer_secret=secret,
+                          access_token_key=token,
+                          access_token_secret=token_secret)
+
+
+        business = client.GetBusiness(
+            u'red-cat-jazz-caf\xe9-houston-3'.encode('utf-8'))
+        assert business.name == u'Red Cat Jazz Caf\xe9'
+
+    def test_business_quoted(self, key, secret, token, token_secret):
+        client = yelp.Api(consumer_key=key,
+                          consumer_secret=secret,
+                          access_token_key=token,
+                          access_token_secret=token_secret)
+
+
+        business = client.GetBusiness(
+            urllib.quote(u'red-cat-jazz-caf\xe9-houston-3'.encode('utf-8')))
+        assert business.name == u'Red Cat Jazz Caf\xe9'
 
     def test_required_location(self, key, secret, token, token_secret):
         client = yelp.Api(consumer_key=key,
